ac2dae57fb081c95fcef2fccef05b28c5a6d2025
Dhaval Kolapkar <dhavalkolapkar@Dhavals-MacBook-Pro.local>
2015-04-08 15:57:50 -0700

    Added inter cluster communication code

---
+++ src/poke/server/conf/ClusterConf.java
@@ -0,0 +1,87 @@
+package poke.server.conf;
+
+import java.util.TreeMap;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import poke.server.conf.ServerConf.AdjacentConf;
+
+@XmlRootElement(name = "conf")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ClusterConf {
+	private int clusterId = -1;
+	private String clusterName;
+	private ClusterNodes clusterNodes;
+
+	@XmlRootElement(name = "adjacent")
+	@XmlAccessorType(XmlAccessType.FIELD)
+	public static final class ClusterNodes {
+		private TreeMap<Integer, NodeDesc> clusterNodes;
+	
+		public NodeDesc getNode(String name) {
+			return clusterNodes.get(name);
+		}
+	
+		public void add(NodeDesc node) {
+			if (node == null)
+				return;
+			else if (clusterNodes == null)
+				clusterNodes = new TreeMap<Integer, NodeDesc>();
+	
+			clusterNodes.put(node.getNodeId(), node);
+		}
+	
+		public TreeMap<Integer, NodeDesc> getAdjacentNodes() {
+			return clusterNodes;
+		}
+	
+		public void setAdjacentNodes(TreeMap<Integer, NodeDesc> nearest) {
+			this.clusterNodes = nearest;
+		}
+	}
+
+	/**
+	 * @return the clusterId
+	 */
+	public int getClusterId() {
+		return clusterId;
+	}
+
+	/**
+	 * @param clusterId the clusterId to set
+	 */
+	public void setClusterId(int clusterId) {
+		this.clusterId = clusterId;
+	}
+
+	/**
+	 * @return the clusterName
+	 */
+	public String getClusterName() {
+		return clusterName;
+	}
+
+	/**
+	 * @param clusterName the clusterName to set
+	 */
+	public void setClusterName(String clusterName) {
+		this.clusterName = clusterName;
+	}
+
+	/**
+	 * @return the clusterNodes
+	 */
+	public ClusterNodes getClusterNodes() {
+		return clusterNodes;
+	}
+
+	/**
+	 * @param clusterNodes the clusterNodes to set
+	 */
+	public void setClusterNodes(ClusterNodes clusterNodes) {
+		this.clusterNodes = clusterNodes;
+	}
+	
+}
---
+++ src/poke/server/managers/ClusterManager.java
@@ -0,0 +1,75 @@
+package poke.server.managers;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import poke.client.ChatClientInitializer;
+import poke.server.ServerInitializer;
+import poke.server.conf.ClusterConf;
+import poke.server.conf.ClusterConf.ClusterNodes;
+import poke.server.conf.NodeDesc;
+import poke.server.conf.ServerConf;
+
+public class ClusterManager {
+	protected static Logger logger = LoggerFactory.getLogger("cluster");
+	private static ClusterConf clusterCfg;
+	protected static AtomicReference<ClusterManager> instance = new AtomicReference<ClusterManager>();
+	
+	public static ClusterManager initManager(ClusterConf conf) {
+		ClusterManager.clusterCfg = conf;
+		instance.compareAndSet(null, new ClusterManager());
+		return instance.get();
+	}
+	
+	public static ClusterManager getInstance() {
+		// TODO throw exception if not initialized!
+		return instance.get();
+	}
+	
+	public static void registerConnections(){
+		TreeMap<Integer, NodeDesc> clusterNodes = clusterCfg.getClusterNodes().getAdjacentNodes();
+			
+		for(Entry<Integer, NodeDesc> entry : clusterNodes.entrySet()) {
+			  Integer key = entry.getKey();
+			  NodeDesc value = entry.getValue();
+
+			  System.out.println(key + " => " + value);
+			  EventLoopGroup group = new NioEventLoopGroup();
+				
+			  try{
+					Bootstrap bootstrap = new Bootstrap()
+					.group(group)
+					.channel(NioSocketChannel.class)
+					.handler(new ServerInitializer(false));
+					
+					Channel channel = bootstrap.connect(value.getHost(),value.getPort()).sync().channel();
+					System.out.println("channel is"+channel.localAddress());
+					/*while(true){
+						channel.write(in.readLine()+ "\r\n");	
+					}*/
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}finally{
+					group.shutdownGracefully();
+				}
+		}
+
+
+	}
+}
