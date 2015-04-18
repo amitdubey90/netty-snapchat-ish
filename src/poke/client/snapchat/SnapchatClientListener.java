2befc4954443de70266c15b7809af2ab0f177f50
Amit Dubey <amit.dubey2219@gmail.com>
2015-04-17 22:59:11 -0700

    modified clientListener

--- src/poke/client/snapchat/SnapchatClientListener.java
+++ src/poke/client/snapchat/SnapchatClientListener.java
@@ -12,39 +12,54 @@ import poke.comm.App.Request;
 import com.google.protobuf.ByteString;
 import java.io.File;
 import java.nio.file.Path;
+
 public class SnapchatClientListener implements CommListener {
 
-    private int clientId;	
+	private int clientId;
 	private int counter;
+
 	@Override
 	public String getListenerID() {
 		// TODO Auto-generated method stub
-		
+
 		return null;
 	}
 
 	public SnapchatClientListener(int clientId) {
-		this.clientId=clientId;
+		this.clientId = clientId;
 	}
-	
+
 	@Override
 	public void onMessage(Request request) {
-		if(request.getHeader().getRoutingId()==Routing.REGISTER){
+		if (request.getHeader().getRoutingId() == Routing.REGISTER) {
 			System.out.println("Client Registered!");
-		}else if(request.getBody().getClientMessage().getMessageType()==MessageType.SUCCESS){
+		} else if (request.getBody().getClientMessage().getMessageType() == MessageType.SUCCESS) {
 			System.out.println("Message sent!");
-		}else{
-			System.out.println("Receiving msg from other client and msg type is: "+request.getBody().getClientMessage().getMessageType());
-			ByteString bs = request.getBody().getClientMessage().getMsgImageBits();
+		}
+		if (request.getBody().hasClientMessage()) {
+			System.out
+					.println("Receiving msg from other client and msg type is: "
+							+ request.getBody().getClientMessage()
+									.getMessageType());
+			ByteString bs = request.getBody().getClientMessage()
+					.getMsgImageBits();
 			byte[] bytes = bs.toByteArray();
 			FileOutputStream fos;
 			try {
-				File f=new File("/Users/dhavalkolapkar/Pictures/SnapchatReceiver/"+clientId);
-				if(!f.exists()){
-					new File("/Users/dhavalkolapkar/Pictures/SnapchatReceiver/"+clientId).mkdirs();
-				};
-				StringBuilder sb = new StringBuilder("/Users/dhavalkolapkar/Pictures/SnapchatReceiver/"+clientId);
-				sb.append(request.getBody().getClientMessage().getMsgImageName());
+				File f = new File("/Users/Amit/Pictures/SnapchatReceiver/"
+						+ clientId);
+				if (!f.exists()) {
+					new File("/Users/Amit/Pictures/SnapchatReceiver/"
+							+ clientId).mkdirs();
+				}
+				;
+				StringBuilder sb = new StringBuilder(
+						"/Users/Amit/Pictures/SnapchatReceiver/" + clientId+"/");
+				sb.append("-"
+						+ System.currentTimeMillis()
+						+ ""
+						+ request.getBody().getClientMessage()
+								.getMsgImageName());
 				fos = new FileOutputStream(sb.toString());
 				fos.write(bytes, 0, bytes.length);
 				fos.flush();
