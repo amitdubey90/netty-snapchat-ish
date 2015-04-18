1812a0b0c38379a8fd2fc7972e350264627b209f
Amit Dubey <amit.dubey2219@gmail.com>
2015-04-18 13:45:06 -0700

    redis store was set off by accident. turning it back on

--- src/poke/server/managers/Raft/LogPersistence.java
+++ src/poke/server/managers/Raft/LogPersistence.java
@@ -73,7 +73,7 @@ public class LogPersistence extends Thread{
 		//TODO improve storing mechanism
 		String temp = "";
 		temp = "PrevLogTerm:" + log.prevLogTerm + "  PrevLogIndex:" + log.prevLogIndex + "  LogTerm:"+log.term+ "  LogIndex:"+log.logIndex +"  ClientRequest:"+log.logData;
-		//j.set(Integer.toString(log.logIndex), temp);
+		j.set(Integer.toString(log.logIndex), temp);
 		lastWrittenLogIndex = log.logIndex;
 		LogManager.logs.remove(log.logIndex);
 	}
